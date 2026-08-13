3cb66c6d446c2c83836dcdceea97f0d419dfc01c
nahid1-2 <nahid71371@gmail.com>
2026-08-13 17:08:48 +0600

    Result calculation

---
+++ test.py
@@ -0,0 +1,15 @@
+number=input("Enter a number: ")
+number=int(number)
+
+
+if number >= 80:
+    print("Grade: A")
+elif number >= 70:
+    print("Grade: B")
+elif number >= 60:
+    print("Grade: C")
+elif number >= 50:
+    print("Grade: D")
+else:
+    print("file")
+print("End")
