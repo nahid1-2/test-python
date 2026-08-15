4853ad5ecc5ca2404dc881b6d11f5b66ae58b2d3
nahid1-2 <nahid71371@gmail.com>
2026-08-15 12:22:24 +0600

    mulitplication

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-number=input("Enter a number: ")
-number=int(number)
+#number=input("Enter a number: ")
+#number=int(number)
 
 # comment
-
+"""
 if number >= 80:
     print("Grade: A")
 elif number >= 70:
@@ -14,3 +14,18 @@ elif number >= 50:
 else:
     print("file")
 print("End")
+"""
+
+def mulitplication (n):
+
+    print(n,"x 1 =", n*1)
+    print(n,"x 2 =", n*2)
+    print(n,"x 3 =", n*3)
+    print(n,"x 4=", n*4)
+    print(n,"x 5=", n*5)
+n= input("Enter Your Number: ")
+n= int(n)
+mulitplication(n)
+mulitplication(n+1)
+mulitplication(n+2)
+mulitplication(n+3)
